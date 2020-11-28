4f0fa0b79e9e431ae5c03db4a781c6da2181bcda
developermatheus <matheus.developer@gmail.com>
2020-11-27 21:58:42 -0300

    Novo componente de UserCard

---
+++ src/components/UserCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card>
+    <v-img :src="user.avatar_url"></v-img>
+    <v-card-title v-if="user.name">Nome: {{ user.name }}</v-card-title>
+    <v-card-title v-if="user.login">User: {{ user.login }}</v-card-title>
+    <v-card-subtitle v-if="user.name">
+      Criado em: {{ user.created_at }}
+    </v-card-subtitle>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "UserCard",
+
+  props: {
+    user: Object,
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,12 +18,56 @@
         />
       </div>
     </div>
+
+    <section class="mt-5">
+      <h2 class="text-center">Digite abaixo o username e clique em buscar</h2>
+      <v-container>
+        <v-row align="center" justify="center">
+          <v-col class="col-12 col-md-6 col-lg-3 col-xl-2">
+            <v-text-field
+              color="primary"
+              v-model="username"
+              label="Username"
+              outlined
+              hide-details="false"
+            ></v-text-field>
+          </v-col>
+          <v-col class="col-12 col-lg-1">
+            <v-btn
+              large
+              block
+              color="primary"
+              :disabled="username.length < 1"
+              @click="getUserByUsername"
+            >
+              Buscar
+            </v-btn>
+          </v-col>
+        </v-row>
+
+        <div class="text-center">
+          <v-progress-circular
+            color="primary"
+            class="mx-auto"
+            v-if="isLoading"
+            indeterminate
+            size="120"
+          ></v-progress-circular>
+        </div>
+
+        <v-row v-if="user" justify="center">
+          <v-col class="col-12 col-md-5 col-lg-3">
+            <UserCard :user="this.user" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import UserCard from "@/components/UserCard.vue";
 import usersService from "@/services/users/index";
 
 export default {
@@ -31,22 +75,27 @@ export default {
   data() {
     return {
       user: null,
+      username: "",
+      isLoading: false,
     };
   },
-  mounted() {
-    this.getUserByUsername("developermatheus");
+  components: {
+    UserCard
   },
   methods: {
-    async getUserByUsername(username) {
+    async getUserByUsername() {
+      this.isLoading = true;
       try {
-        const { data } = await usersService.getUser(username);
+        const { data } = await usersService.getUser(this.username);
         console.log(data);
         if (data) {
           this.user = data;
         }
       } catch (error) {
         // this.$showSnackbar(error.message);
-        console.log("erro ao pegar da api")
+        console.log("erro ao pegar da api");
+      } finally {
+        this.isLoading = false;
       }
     },
   },
